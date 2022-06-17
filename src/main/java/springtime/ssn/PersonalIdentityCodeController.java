20405c9564d39550229f70ff0a24f12c45f8b50b
jarrykinn <107424889+jarrykinn@users.noreply.github.com>
2022-06-17 10:23:25 +0300

    slight rollback

--- src/main/java/springtime/ssn/PersonalIdentityCodeController.java
+++ src/main/java/springtime/ssn/PersonalIdentityCodeController.java
@@ -3,16 +3,22 @@ package springtime.ssn;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.*;
-import springtime.exhangerates.ExchangeRate;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- *
+ * Controller to expose endpoint for validating Social Security Number(s) SSN.
+ * 		POST /validate_ssn
+ *         {
+ *           "ssn": "131052-308T",
+ *           "countryCode": "FI"
+ *         }
  */
 @RestController
 class PersonalIdentityCodeController {
@@ -22,16 +28,11 @@ class PersonalIdentityCodeController {
 		dataBinder.setDisallowedFields("id");
 	}
 
-	@PostMapping("/validate_ssn/body")
+	@PostMapping("/validate_ssn")
 	public ResponseEntity validateSsnBody(@RequestBody @Valid PersonalIdentityCode ssn) {
 		System.out.println("SSN: " + ssn.getSsn());
 		ssn.setSsn_valid(true);
 		return ResponseEntity.ok().body(ssn);
-	}
-
-	@PostMapping("/validate_ssn")
-	public ResponseEntity validateSsnParams(@RequestParam String ssn, @RequestParam String country_code) {
-		return ResponseEntity.ok().build();
 	}
 
 	private ResponseEntity respondError(HttpStatus status, String message) {
